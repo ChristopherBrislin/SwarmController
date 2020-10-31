b23124744b9a70925b230b2c704b51ec07dff469
Christopher <brislinchris@gmail.com>
2020-10-31 21:44:07 +1100

    Initial Commit

---
+++ SwarmController/src/Main.java
@@ -0,0 +1,26 @@
+import com.fazecast.jSerialComm.SerialPort;
+
+/**
+ * 
+ */
+
+/**
+ * Christopher Brislin
+ * 31 Oct 2020
+ * SwarmController
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		SerialPort[] portList = SerialPort.getCommPorts();
+		String[] portNames = new String[] {};
+		
+		
+
+	}
+
+}
