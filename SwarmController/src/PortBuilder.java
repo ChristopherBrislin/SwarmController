b808cfbd9afaa28239f1d3fcd68d3a77353ed069
ChristopherBrislin <65586758+ChristopherBrislin@users.noreply.github.com>
2020-11-08 11:24:45 +1100

    MavlinkMessageInfo to parse message contents

--- SwarmController/src/PortBuilder.java
+++ SwarmController/src/PortBuilder.java
@@ -2,7 +2,9 @@ import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -15,7 +17,10 @@ import io.dronefleet.mavlink.Mavlink2Message;
 import io.dronefleet.mavlink.MavlinkConnection;
 import io.dronefleet.mavlink.MavlinkDialect;
 import io.dronefleet.mavlink.MavlinkMessage;
+import io.dronefleet.mavlink.annotations.MavlinkFieldInfo;
 import io.dronefleet.mavlink.annotations.MavlinkMessageInfo;
+import io.dronefleet.mavlink.ardupilotmega.ArdupilotmegaDialect;
+import io.dronefleet.mavlink.common.BatteryStatus;
 import io.dronefleet.mavlink.common.Heartbeat;
 import io.dronefleet.mavlink.common.MavAutopilot;
 import io.dronefleet.mavlink.common.Statustext;
@@ -23,6 +28,7 @@ import io.dronefleet.mavlink.common.StatustextLong;
 import io.dronefleet.mavlink.common.SysStatus;
 import io.dronefleet.mavlink.protocol.MavlinkPacket;
 import io.dronefleet.mavlink.protocol.MavlinkPacketReader;
+import io.dronefleet.mavlink.serialization.payload.MavlinkPayloadDeserializer;
 import io.dronefleet.mavlink.standard.StandardDialect;
 
 /**
@@ -53,7 +59,7 @@ public class PortBuilder {
 		this.port.openPort();
 		
 		port.addDataListener(new SerialPortDataListener() {
-			float last = 0;
+			float last = -1;
 			float dropCount = 0;
 			float totalCount = 0;
 			   @Override
@@ -67,15 +73,17 @@ public class PortBuilder {
 			      try {
 			    	  	
 						MavlinkConnection connection = MavlinkConnection.builder(port.getInputStream(), port.getOutputStream())
-								.dialect(MavAutopilot.MAV_AUTOPILOT_GENERIC, new StandardDialect())
+								.dialect(MavAutopilot.MAV_AUTOPILOT_ARDUPILOTMEGA, new ArdupilotmegaDialect())
 								.build();
 						
 						MavlinkMessage message;
-						StandardDialect dialect = new StandardDialect();
+						MavlinkPacket packet; 
+						
 						
 						while ((message = connection.next()) != null) {
 							if(message.getSequence()!= (last+1)) {
-								dropCount += message.getSequence() - last;
+								//dropCount += message.getSequence() - last;
+								dropCount++;
 							}
 							totalCount ++;
 							
@@ -87,18 +95,33 @@ public class PortBuilder {
 								//droneList.add(drone);
 								droneMap.put(id, drone);
 							}
-							System.out.println((dropCount/totalCount*100) + "% \t" + dropCount + "\t" + message.getPayload());
 							
 							
-							if(message.getPayload() instanceof Heartbeat) {
-								MavlinkMessage<Heartbeat> heartbeat = (MavlinkMessage<Heartbeat>) message;
-								heartbeat.getOriginSystemId();
+							
+							
+							
+							
+							//int msgID = ((MavlinkMessageInfo)message.getPayload().getClass().getAnnotation(MavlinkMessageInfo.class)).id();
+							//System.out.println(dropCount + "% \t" + last + "\t" + msgID +"\t" + message.getPayload());
+							if(message.getPayload() instanceof Statustext) {
+								MavlinkMessage<Statustext> status = (MavlinkMessage<Statustext>) message;
+								final Object message2 = status.getPayload();
 								
-								//int test = status.hashCode();
-								//System.out.println(dropCount+ "\t" + last + "\t" +status.getPayload());
-								//System.out.println(status.getPayload());
+								Arrays.stream(message2.getClass().getDeclaredMethods())
+								.filter(f -> f.isAnnotationPresent(MavlinkFieldInfo.class))
+								.forEach(f -> {
+									try {
+										System.out.printf("%s = %s\n", f.getName(), f.invoke(message2).toString());
+									} catch(InvocationTargetException | IllegalAccessException e) {
+										e.printStackTrace();
+									}
+									
+								});
+							
 							}
 							last = message.getSequence();
+							if(last == 255) last = -1; //Sequence wraparround 
+							
 						}
 					} catch (Exception ex) {
 						ex.printStackTrace();
