3a2530561a6eb91181ed9fd969b1e8f2879ffd4c
ChristopherBrislin <65586758+ChristopherBrislin@users.noreply.github.com>
2021-01-10 18:32:22 +1100

    Screen size query and Colour pallette

--- SwarmController/src/Const.java
+++ SwarmController/src/Const.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Dimension;
 
 /**
@@ -10,6 +11,22 @@ import java.awt.Dimension;
  * SwarmController
  */
 public class Const {
-	static final Dimension DEFAULT_MIN_SIZE = new Dimension(1007,500);
+	//
+	static Dimension screenSize;
+	
+	//Launch Windo Size
+	static Dimension DEFAULT_MIN_SIZE = new Dimension(1007,500);
+	
+	//Interface Colour Scheme
+	static final Color WARNING = new Color(245, 66, 66);
+	static final Color CAUTION = new Color(252, 177, 3);
+	static final Color OPTIMAL = new Color(34, 204, 0);
+	
+	public static void setScreenSize(Dimension size) {
+		Const.screenSize = size;
+		
+		System.out.println(screenSize);
+		
+	}
 
 }
